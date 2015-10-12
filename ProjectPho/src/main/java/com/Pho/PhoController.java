5691386633681864f208ac6465947f36be0c59f1
Jiawei Huang <jhujhuang@gmail.com>
2015-10-12 18:56:44 -0400

    PhoController.java javadoc

--- ProjectPho/src/main/java/com/Pho/PhoController.java
+++ ProjectPho/src/main/java/com/Pho/PhoController.java
@@ -7,25 +7,30 @@ import java.util.Collections;
 
 import static spark.Spark.post;
 
+/**
+ * The controller of the MVC model.
+ */
 public class PhoController {
 
-    private static final String API_CONTEXT = "/Pho/api/sessions";
+    private static final String API_CONTEXT = ""; // TODO: confirm
 
     private final PhoService PhoService;
 
     private final Logger logger = LoggerFactory.getLogger(PhoController.class);
+
+    /**
+     * Initializes the controller with PhoService
+     * @param PhoService a PhoService instance
+     */
     public PhoController(PhoService PhoService) {
         this.PhoService = PhoService;
         setupEndpoints();
     }
+
+    /**
+     * Sets up the endpoints.
+     */
     private void setupEndpoints() {
-        // start a game
-        post(API_CONTEXT, "application/json", (request, response) -> {
-
-                logger.error("Failed to create new game");
-                System.out.println("Error occurred when starting game: failed to create new game!");
-                return Collections.EMPTY_MAP;
-        }, new JsonTransformer());
-
+        // TODO: implement
     }
 }
