ffc93b6b76c96d3c35655c8e0143a30f2e671887
Jiawei Huang <jhujhuang@gmail.com>
2015-10-12 18:05:11 -0400

    javadoc of Comment.java & fix typo in User.java

--- ProjectPho/src/main/java/com/Pho/Comment.java
+++ ProjectPho/src/main/java/com/Pho/Comment.java
@@ -1,4 +1,47 @@
 package com.Pho;
 
+/**
+ * A Comment on a photo in the edit interface.
+ */
 public class Comment {
+    private String content;
+    private String author;
+    private String createdTime;
+
+    /**
+     * Creates a Comment instantce.
+     * @param content the actual content that the user has entered.
+     * @param author the userId of the author.
+     * @param createdTime string representing the time of comment.
+     */
+    public Comment(String content, String author, String createdTime) {
+        // TODO: implement
+    }
+
+    /**
+     * Retrieves the content of the comment.
+     * @return string of the content
+     */
+    public String getContent() {
+        // TODO: implement
+        return null;
+    }
+
+    /**
+     * Retrieves the author of the comment.
+     * @return string of user id of the author
+     */
+    public String getAuthor() {
+        // TODO: implement
+        return null;
+    }
+
+    /**
+     * Retrieves the created time of the comment.
+     * @return string of the time that the comment was created.
+     */
+    public String getCreatedTime() {
+        // TODO: implement
+        return null;
+    }
 }
--- ProjectPho/src/main/java/com/Pho/User.java
+++ ProjectPho/src/main/java/com/Pho/User.java
@@ -1,7 +1,7 @@
 package com.Pho;
 
 public class User {
-    private String userName, userEMail;
+    private String userName, userEmail;
     private Photo [] album;
 
     /** Constructor for the User class
@@ -9,14 +9,14 @@ public class User {
      * @param userEmail email of the user
      */
     public User(String userName, String userEmail) {
-	this.album = new Photo[1];
+        this.album = new Photo[1];
     }
 
     /** Get the user name of user.
      * @return the user name of the user
      */
     public String getUserName() {
-	return this.userName;
+        return this.userName;
     }
 
 
