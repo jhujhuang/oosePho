2df2cdd729b34d2ffb8602351ba45889d8979ad1
David Hsieh <thsieh10@jhu.edu>
2015-10-12 18:11:53 -0400

    Versoin.java javadoc

--- ProjectPho/src/main/java/com/Pho/User.java
+++ ProjectPho/src/main/java/com/Pho/User.java
@@ -1,10 +1,14 @@
 package com.Pho;
 
+/**
+ * The Use class is for holding information of user.
+ */
 public class User {
     private String userName, userEMail;
     private Photo [] album;
 
-    /** Constructor for the User class
+    /**
+     * Constructor for the User class
      * @param userName user name of the user
      * @param userEmail email of the user
      */
@@ -12,7 +16,8 @@ public class User {
 	this.album = new Photo[1];
     }
 
-    /** Get the user name of user.
+    /**
+     * Get the user name of user.
      * @return the user name of the user
      */
     public String getUserName() {
@@ -20,13 +25,13 @@ public class User {
     }
 
 
-    /** Get the user email of this user.
+    /**
+     * Get the user email of this user.
      * @return the email of the user
      */
     public String getUserEmail() {
 	return this.userEmail;
     }
-
 
     /** Get the list of photos of the user.
      * @return the array containing the photos of the user
--- ProjectPho/src/main/java/com/Pho/Version.java
+++ ProjectPho/src/main/java/com/Pho/Version.java
@@ -1,9 +1,48 @@
 package com.Pho;
 
+import java.awt.image.BufferedImage;
 
 /**
  * Version class contains the actual picture of photo.
+ * A Photo class contains a list of versions.
  */
 public class Version {
-    public 
+    private String versionId, createdTime;
+    private BufferedImage img;
+    
+    /**
+     * Constructor for the Version class.
+     * @param versionId the id of this version
+     * @param createdTime the createdTime of this version
+     * @param img the image of this version
+     */
+    public Version(String versionId, String createdTime, BufferedImage img) {
+	this.versionId = versionId;
+	this.createdTime = createdTime;
+	this.img = img;
+    }
+
+    /**
+     * Get the versionId of this version.
+     * @return the version id of this version
+     */ 
+    public String getVersionId() {
+	return this.versionId;
+    } 
+
+    /**
+     * Get the created time of this version.
+     * @return the created time of this version
+     */
+    public String getCreatedTime() {
+	return this.createdTime;
+    }
+
+    /**
+     * Get the image of this version.
+     * @return the image of this verison
+     */
+    public BufferedImage getImage() {
+	return this.img;
+    }
 }
