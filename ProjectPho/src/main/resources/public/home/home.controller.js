37d225eb9d4614c8e5c3bd17498b4b20b212616c
Meteorite-J <JoeyCode1@gmail.com>
2015-12-02 12:52:39 -0500

    error messages.

--- ProjectPho/src/main/resources/public/home/home.controller.js
+++ ProjectPho/src/main/resources/public/home/home.controller.js
@@ -84,10 +84,20 @@
                 transformRequest: angular.identity
             })
             .success(function(){
-                // alert("Upload success!");
+                console.log("Upload success.");
             })
             .error(function(){
+                console.log("Failed to send the image to server!");
             });
+        }
+
+        function openImage(){
+            /* Check for the various File API support.*/
+            if (window.File && window.FileReader && window.FileList && window.Blob) {
+              // Great success! All the File APIs are supported.
+            } else {
+              alert('The File APIs are not fully supported in this browser.');
+            }
         }
     }
 
